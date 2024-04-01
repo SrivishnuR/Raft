81bcf5fffd4a435fb77d3d3e4b91324d7c3613ea
Srivishnu Ramamurthi <srivishnur@ucla.edu>
2024-03-31 19:44:29 -0700

    feat: basics of part 1

--- raft/src/bin/async_server.rs
+++ raft/src/bin/async_server.rs
@@ -1,37 +1,37 @@
-use raft::{read, send_message};
+use raft::{async_read, async_send_message};
 use std::collections::HashMap;
 use std::sync::Arc;
-use std::sync::Mutex;
-use std::thread;
-use std::{
-    error::Error,
-    net::{Ipv4Addr, TcpListener},
-};
+use std::{error::Error, net::Ipv4Addr};
+use tokio::net::TcpListener;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 5555)).unwrap();
-    let data = Arc::new(Mutex::new(HashMap::new()));
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 5555))
+        .await
+        .unwrap();
+    let data = Arc::new(tokio::sync::Mutex::new(HashMap::new()));
 
     // accept connections and process them serially
-    for wrapped_stream in listener.incoming() {
-        let data = Arc::clone(&data);
-        thread::spawn(move || {
-            if let Ok(mut stream) = wrapped_stream {
-                loop {
-                    let message = read(&mut stream).unwrap();
-                    let data = Arc::clone(&data);
-                    let response = parse_message(message, data);
+    loop {
+        let (mut stream, _) = listener.accept().await.unwrap();
 
-                    send_message(&mut stream, &response).unwrap();
-                }
+        let data = Arc::clone(&data);
+        tokio::spawn(async move {
+            loop {
+                let message = async_read(&mut stream).await.unwrap();
+                let data = Arc::clone(&data);
+                let response = parse_message(message, data).await;
+
+                async_send_message(&mut stream, &response).await.unwrap();
             }
         });
     }
-
-    Ok(())
 }
 
-fn parse_message(message: String, data: Arc<Mutex<HashMap<String, String>>>) -> String {
+async fn parse_message(
+    message: String,
+    data: Arc<tokio::sync::Mutex<HashMap<String, String>>>,
+) -> String {
     let sectioned_message: Vec<&str> = message.trim().split(' ').collect();
 
     if sectioned_message.len() <= 1 {
@@ -44,7 +44,7 @@ fn parse_message(message: String, data: Arc<Mutex<HashMap<String, String>>>) -> 
                 return String::from("Invalid command");
             }
 
-            let mut data = data.lock().unwrap();
+            let mut data = data.lock().await;
             data.insert(
                 String::from(sectioned_message[1]),
                 String::from(sectioned_message[2]),
@@ -56,7 +56,7 @@ fn parse_message(message: String, data: Arc<Mutex<HashMap<String, String>>>) -> 
                 return String::from("Invalid command");
             }
 
-            let data = data.lock().unwrap();
+            let data = data.lock().await;
             if let Some(entry) = data.get(sectioned_message[1]) {
                 return entry.to_owned();
             }
@@ -68,7 +68,7 @@ fn parse_message(message: String, data: Arc<Mutex<HashMap<String, String>>>) -> 
                 return String::from("Invalid command");
             }
 
-            let mut data = data.lock().unwrap();
+            let mut data = data.lock().await;
             if let Some(_) = data.remove(sectioned_message[1]) {
                 return "Deleted successfully".to_owned();
             }
--- raft/src/bin/client.rs
+++ raft/src/bin/client.rs
@@ -1,4 +1,5 @@
-use raft::send_message;
+use raft::{read, send_message};
+use std::io::Write;
 use std::net::Ipv4Addr;
 use std::{error::Error, io, net::TcpStream};
 
@@ -6,9 +7,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut stream = TcpStream::connect((Ipv4Addr::LOCALHOST, 5555))?;
 
     loop {
+        print!("KV > ");
+        io::stdout().flush().unwrap();
+
         let mut user_input = String::new();
         let stdin = io::stdin();
         stdin.read_line(&mut user_input)?;
         send_message(&mut stream, &user_input)?;
+        let response = read(&mut stream).unwrap();
+        println!("{}", response);
     }
 }
